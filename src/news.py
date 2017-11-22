7426211f675a9237c7dc9c76074822efa7fa9b54
okano <okanosyogo@gmail.com>
2017-11-22 15:40:34 +0900

    天気とニュースのプログラムを追加

---
+++ src/news.py
@@ -0,0 +1,32 @@
+import requests
+from bs4 import BeautifulSoup
+def GetNews():
+    url = 'https://news.finance.yahoo.co.jp/'
+    res = requests.get(url)
+
+    # print(res.text)
+    soup = BeautifulSoup(res.text, 'lxml') #要素を抽出
+
+    '''
+    #リンクを表示
+    for a in soup.find_all('a'):
+        text = a.get('href')
+        print(text)         
+    '''
+    res = []
+    explains = soup.find_all("li", {"class": "ymuiArrow1"})
+    for explain in explains:
+        res.append(explain.text.replace("\xa0",""))
+        res.append("\n")
+    print(res)
+    
+    # main_body = soup.find("div", {"id": "main"})
+    
+    # f.writelines(sub_body.prettify())
+    
+    # main_body = soup.find("div", {"id": "main"})
+    # print(main_body)
+    # f.writelines(res.text) # シーケンスが引数。
+    
+if __name__=="__main__":
+    GetNews()
--- src/weather.py
+++ src/weather.py
@@ -1,29 +1,32 @@
-from requests_oauthlib import OAuth1Session
+#from requests_oauthlib import OAuth1Session
 #import sys
 #sys.path.append("C:\\Users\\okano\\Anaconda3\\Lib\\site-packages")
-from datetime import datetime
-import locale
-import time
-import threading
+#from datetime import datetime
+#import locale
+#import time
+#import threading
 import json
 import requests
 from bs4 import BeautifulSoup
+import jtalk
 
-def LoopFunc():
-    WeatherFunc()
-'''
-    b = datetime.now()
-    if(b.minute%5 == 0): # x分ごとの時報
-        WeatherFunc()
-    # 呼び出された後の処理
-    # 指定秒に呼び出されるようにスレッドをセットする
-    t=threading.Timer(60,LoopFunc)
-    t.start()
-'''
+def SayWeather():
+# 内容：天気と気温を読み上げる
+# TODO 語尾や言い方にバリエーションを持たせるような造り込みがあるとおもしろい
+    jtalk.jtalk("今日の天気をお伝えします")
+    list_ = WeatherFunc()
+    jtalk.jtalk("今日の天気は" + list_[0])
+    if list_[1] != "不明":
+        jtalk.jtalk("最高気温は" + list_[1] + "度")
+    else:
+        jtalk.jtalk("最高気温は" + list_[1] + "です")
+        
+    if list_[2] != "不明":
+        jtalk.jtalk("最低気温は" + list_[2] + "度")
+    else:
+        jtalk.jtalk("最低気温は" + list_[2] + "です")
 def WeatherFunc():
-    # try:
-    # 文字化け確認のため、ファイルに出力する
- #   f = open('text.txt', 'w') # 書き込みモードで開く
+# 内容：天気に関するデータをweatherhackのAPIから手に入れる
     # jsonデータを取ってくる
     url = "http://weather.livedoor.com/forecast/webservice/json/v1?city=170010"
     resp = requests.get(url)
@@ -34,21 +37,23 @@ def WeatherFunc():
     telop = json_dict['forecasts'][0]['telop']
 
     try:
-        tem_min = json_dict['forecasts'][0]['temperature']['min']['celsius']
-        tem_min += '°'
-    except TypeError:
-        tem_min = "不明"
-
-    try:
         tem_max = json_dict['forecasts'][0]['temperature']['max']['celsius']
         tem_max += '°'
     except TypeError:
         tem_max = "不明"
 
-    out_str = "天気 : " + telop + "\n" + "最高気温 : " + tem_max + "\n" + "最低気温 : " + tem_min
+    try:
+        tem_min = json_dict['forecasts'][0]['temperature']['min']['celsius']
+        tem_min += '°'
+    except TypeError:
+        tem_min = "不明"
 
-    return out_str
+    output = [telop,tem_max,tem_min]
+#    out_str = "天気 : " + telop + "\n" + "最高気温 : " + tem_max + "\n" + "最低気温 : " + tem_min
+#    out_str = "天気は" + telop + "最高気温は" + tem_max + "最低気温は" + tem_min
+
+    return output
 
 if __name__=="__main__":
-    jtalk.jtalk(WeatherFunc())
-
+#    jtalk.jtalk(WeatherFunc())
+    SayWeather()
