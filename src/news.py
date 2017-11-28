67d96b4fdff333ebf393112b9dffa69490bb9d09
okano <okanosyogo@gmail.com>
2017-11-28 13:17:22 +0900

    ニュースまわりの構造を大幅にかえる前のヴァージョン

--- src/news.py
+++ src/news.py
@@ -1,26 +1,27 @@
 import requests
 from bs4 import BeautifulSoup
 #news_list
-def GetNews():
-    if 1:
-        url = 'https://news.finance.yahoo.co.jp/'
-    else:
-        url = 'https://news.yahoo.co.jp/pickup/economy/rss.xml'
-
-    res = requests.get(url)
+def NewsMaster():
+    news_url = SelectNews()
+    GetNewsList(news_url)
+    GetNewsText()
+def SelectNews():
+    url = 'https://news.finance.yahoo.co.jp/'
+#    url = 'https://news.yahoo.co.jp/pickup/economy/rss.xml'
+#    url = 'https://rdsig.yahoo.co.jp/rss/l/headlines/sci/it_nlab/RV=1/RU=aHR0cHM6Ly9oZWFkbGluZXMueWFob28uY28uanAvaGw_YT0yMDE3MTEyNC0wMDAwMDA3My1pdF9ubGFiLXNjaQ--'
+    return url
+def GetNewsText():
+    pass
+def GetNewsList(url_):
+    res = requests.get(url_)
 
     # print(res.text)
     soup = BeautifulSoup(res.text, 'lxml') #要素を抽出
-    print(soup.text)
-
-    '''
-    #リンクを表示
-    for a in soup.find_all('a'):
-        text = a.get('href')
-        print(text)         
-    '''
+#    print(soup.text)
 
     explains = soup.find_all("li", {"class": "ymuiArrow1"})
+#    explains = soup.find_all("p", {"class": "ynDetailText"})
+
     news_list = []
     for explain in explains:
         # 新しく追加する辞書を構成する
@@ -38,33 +39,5 @@ def GetNews():
         print("link :" + tmp["link"])
         print("text :" + tmp["text"])
 
-def PlayGround():    
-    url = 'https://news.yahoo.co.jp/pickup/economy/rss.xml'
-
-    res = requests.get(url)
-
-#    print(res.text)
-    soup = BeautifulSoup(res.text, 'xml') #要素を抽出
-    print(soup.text)
-
-    explains = soup.find_all("div", {"id": "webkit-xml-viewer-source-xml"})
-    #explains = soup.find_all()
-    news_list = []
-    for explain in explains:
-        # 新しく追加する辞書を構成する
-        dict_ = {}
-        dict_ = {"title":"","text":"","genre":"","link":""}
-        dict_["title"] = explain.text.replace("\xa0","")
-        dict_["text"] = ""
-        news_list.append(dict_)
-
-    # 入手したデータを画面に出力
-    for tmp in news_list:
-        print("title:" + tmp["title"])
-        print("genre:" + tmp["genre"])
-        print("link :" + tmp["link"])
-        print("text :" + tmp["text"])
-     
 if __name__=="__main__":
-    PlayGround()
-#    GetNews()
+    NewsMaster()
--- src/rss.py
+++ src/rss.py
@@ -1,13 +1,20 @@
 import feedparser
 from datetime import datetime
 from time import mktime
-def Rss():
+def RssMahter():
+# 音声でジャンルを指定したり、ランダムなジャンルからニュースを取得したり、時間帯によって取得するジャンルを変えたりする機能を予定
+    url = SerectRss()
+    GetRss(url)
+def SerectRss():
+    url = ""
     #RSSのURL
-    RSS_URL  = "https://news.yahoo.co.jp/pickup/economy/rss.xml"
-#    RSS_URL  = "http://www.japantoday.com/feed/"
-
+    url  = "https://news.yahoo.co.jp/pickup/economy/rss.xml"
+#    rss_url  = "http://www.japantoday.com/feed/"
+    url = "https://headlines.yahoo.co.jp/rss/it_nlab-c_sci.xml"
+    return url
+def GetRss(rss_url_):
     #RSSの取得
-    feed = feedparser.parse(RSS_URL)
+    feed = feedparser.parse(rss_url_)
 
     #RSSのタイトル
     print (feed.feed.title)
@@ -15,8 +22,13 @@ def Rss():
     news_list = []
     for entry in range(len(feed.entries)):
         #RSSの内容を一件づつ処理する
+        # TODO titleでかっことその中身を消す
         title = feed.entries[entry].title
         link = feed.entries[entry].link
+        try:
+            category = feed.entries[entry].category
+        except AttributeError:
+            pass
 
         #更新日を文字列として取得
         published_string = feed.entries[entry].published
@@ -33,17 +45,21 @@ def Rss():
 
         # 新しく追加する辞書を構成する
         dict_ = {}
-        dict_ = {"title":"","text":"","genre":"","link":""}
+        dict_ = {"title":"","text":"","link":"","category":""}
         dict_["title"] = title
         dict_["link"] = link
+        try:
+            dict_["category"] = category
+        except:
+            pass
         news_list.append(dict_)
 
     # 入手したデータを画面に出力
     for tmp in news_list:
         print("title:" + tmp["title"])
-        print("genre:" + tmp["genre"])
         print("link :" + tmp["link"])
+        print("cate :" + tmp["category"])
         print("text :" + tmp["text"])
  
 if __name__=="__main__":
-    Rss()
+    RssMahter()
