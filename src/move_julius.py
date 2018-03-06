3d795f9d90fdba1022870a217f47689c7e6ba7b6
okano_KES <okanosyogo@gmail.com>
2018-03-06 05:31:58 +0000

    juliusをpythonから動かすための部分を書いているが、juliusから渡される文字列に含まれる\nが検知できない

--- src/move_julius.py
+++ src/move_julius.py
@@ -9,7 +9,16 @@ import subprocess
 from time import sleep
 import sys
 import urllib
-import xml.etree.ElementTree as ET
+#import xml.etree.ElementTree as ET
+
+import xmltodict
+from xml.parsers.expat import ExpatError,errors
+import json
+
+HOST = 'localhost'
+PORT = 10500
+BUFSIZE = 4096
+
 def command(_com):
     try:
         # コマンドを実行する
@@ -38,6 +47,7 @@ def invoke_julius():
 
 
 def main():
+    # Juliusの起動スクリプトを実行する
     #activate_command = "ALSADEV=\"plughw:1,0\" julius -C ~/grammar-kit-4.3.1/testmic.jconf -gram ~/dict/greeting -nostrip -module"
     #activate_command = ["ALSADEV=\"plughw:1,0\"","julius","-C","~/grammar-kit-4.3.1/testmic.jconf","-gram","~/dict/greeting","-nostrip","-module"]
 
@@ -47,23 +57,58 @@ def main():
     #sleep(3)
 #    invoke_julius()
 
-    host = 'localhost'
-    port = 10500
-    bufsize = 409600
-    
-    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    # ソケット通信の接続を行う
+    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-    # TCPサーバー接続の初期化
-    sock.connect((host,port))
+    try:
+        # TCPサーバー接続の初期化
+        client.connect((HOST,PORT))
+    except:
+        print("ERRER :ソケット接続のエラーが発生したため終了します")
+        sys.exit(0)
 
     # ソケット接続に関連した文書を作成する。
-    sf = sock.makefile('rb')
+    sf = client.makefile('rb')
 
-    while True:
-        # TCPデータを受信するまで待機し、受信データを文字列として返す
-        recv_data = sock.recv(bufsize)
-        print(recv_data)
-        print("!!!!!!")
+    try:
+        # サーバからのデータ受信
+        print("データ受信を開始します")
+        data = ""
+        while 1:
+            #print(client.recv(BUFSIZE)) # test
+            #data = data + client.recv(BUFSIZE)
+            data = data + str(client.recv(BUFSIZE))
+            # TCPデータを受信するまで待機し、受信データを文字列として返す
+#            print("client : ",data)
+
+            if "INPUT" in data:
+                print("インプットってはいってたよ")
+
+            #if "</RECOGOUT>\n." in data: TODO \n.省略したらifの中に入るようになったけど、これで本当に大丈夫か？
+            if "</RECOGOUT>" in data:
+                print("録音終了したので、一旦クリア！！")
+                # RECOGOUT要素以下をXMLとしてパース
+                # root = ET.fromstring('<?xml version="1.0"?>\n' + data[data.find("<RECOGOUT>"):].replace("\n.", ""))
+                # 現状ここで起こるエラーは\nを認識できていないためなので、文字コードとかを確認してutf-8にエンコードして、ここでのエラーが消えるかを見る
+                xml = '<?xml version="1.0"?>\n' + data[data.find("<RECOGOUT>"):].replace("\n.", "")
+                print(xml)
+                dict_ = xmltodict.parse(xml)
+                print(json.dumps(dict_,indent=2))
+
+"""
+<RECOGOUT>\n  <SHYPO RANK="1" SCORE="-2302.836426" GRAM="0">\n    <WHYPO WORD="[s]" CLASSID="4" PHONE="silB" CM="1.000"/>\n    <WHYPO WORD="\xe3\x81\x8a\xe3\x81\xaf\xe3\x82\x88\xe3\x81\x86" CLASSID="0" PHONE="o h a y o u" CM="0.902"/>\n    <WHYPO WORD="[/s]" CLASSID="5" PHONE="silE" CM="0.755"/>\n  </SHYPO>\n</RECOGOUT>\n.\n'
+"""
+
+                # 言葉を判別してどうこうするコードをここに書く
+                data = ""
+
+    except KeyboardInterrupt:
+        # Ctrl+Cとか押されたとき
+        print("keyInterrupt Occured.")
+        # TODO juliusのプロセス終了
+        # julius にkillコマンドを送る(web情報では、killによって終了しないjuliusのバグがあるらしい)
+        client.close
+        sys.exit(0)
 
 #        line = sf.readline().decode('utf-8')
 #        print (line)
@@ -77,9 +122,5 @@ def main():
 #            print(root.tag,root.attrib)
 
 if __name__ == '__main__':
-    try:
-        main()
-    except KeyboardInterrupt:
-        print("keyInterrupt")
-        # TODO ソケット閉じる
-        sys.exit(0)
+    main()
+
